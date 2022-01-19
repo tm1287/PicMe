c243e3f1a72a4893d5fa3e462f8b79173581a7f9
tm1287 <tejas.maraliga@gmail.com>
2022-01-18 22:50:39 -0600

    Add xcode project to repo

---
+++ frontend/PicMe/PicMe/PicMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicMeApp.swift
+//  PicMe
+//
+//  Created by Tejas Maraliga on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PicMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
